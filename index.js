c07faa68f54afe894975ee24a2ce751f715ac150
kharichandra <kharichandra@dminc.com>
2024-01-11 18:22:09 +0530

    update in code

--- index.js
+++ index.js
@@ -2,20 +2,25 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-const PORT = 3000;
+const port = 5000;
 
 // Middleware to parse JSON requests
 app.use(bodyParser.json());
+// Handling GET /hello request 
+app.get("/hello", (req, res, next) => { 
+    res.send("This is the hello response"); 
+}) 
 
-app.post('/webhook-endpoint', (req, res) => {
-    console.log('Webhook received:', req.body);
+// Endpoint to receive data from Wokwi simulator
+app.post('/receive-data', (req, res) => {
+    const dataFromSimulator = req.body;
+    console.log('Received data from Wokwi simulator:', dataFromSimulator);
 
-    // Handle the webhook data as needed
-    // For example, you could trigger an action or send a response
+    // Process the received data, e.g., store in a database, trigger actions, etc.
 
-    res.status(200).send('Webhook received successfully');
+    res.status(200).send('Data received successfully');
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(port, () => {
+    console.log(`Backend server running on http://localhost:${port}`);
 });
